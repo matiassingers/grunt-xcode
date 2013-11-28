9e525e9569b3a6e1957e0f01aa11509702be533e
Matias Singers <mail@matiassingers.com>
2013-11-28 16:18:57 +0800

    Removed generic task code from Grunt plugin init template

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,22 +30,10 @@ module.exports = function(grunt) {
 
     // Configuration to be run (and then tested).
     xcode: {
-      default_options: {
+      default: {
         options: {
-        },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!',
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
+        }
+      }
     },
 
     // Unit tests.
--- tasks/xcode.js
+++ tasks/xcode.js
@@ -5,7 +5,6 @@
  * Copyright (c) 2013 Matias Singers
  * Licensed under the MIT license.
  */
-
 'use strict';
 
 module.exports = function(grunt) {
@@ -14,37 +13,6 @@ module.exports = function(grunt) {
   // creation: http://gruntjs.com/creating-tasks
 
   grunt.registerMultiTask('xcode', 'Compile and build Xcode project with Grunt', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
   });
-
 };
