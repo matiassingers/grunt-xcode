a30bf6d2a15d490222e7030f885a140bb50ff479
Matias Singers <mail@matiassingers.com>
2013-11-28 17:29:37 +0800

    Added String.prototype.format as suggested by @soerenr

--- tasks/xcode.js
+++ tasks/xcode.js
@@ -7,6 +7,15 @@
  */
 'use strict';
 
+String.prototype.format = function () {
+    var formatted = this;
+    for (var i = 0; i < arguments.length; i++) {
+        formatted = formatted.replace(
+            RegExp('\\{' + i + '\\}', 'g'), arguments[i]);
+    }
+    return formatted;
+};
+
 var inquirer = require('inquirer'),
     exec = require('child_process').exec;
 
@@ -23,27 +32,28 @@ module.exports = function(grunt) {
         });
 
     // Check if required gem is installed
-    grunt.verbose.writeln('Checking if ' + gem + ' gem is installed');
-    var child = exec('gem list ' + gem + ' -i', function(error, stdout, stderr){
-      if (error == null) {
-        grunt.verbose.error(gem + ' gem is not installed, prompting for install');
+    grunt.verbose.writeln('Checking if {0} gem is installed'.format(gem));
+    var child = exec('gem list {0} -i'.format(gem), function(error, stdout, stderr){
+      if (error !== null) {
+        grunt.verbose.error('{0} gem is not installed, prompting for install'.format(gem));
         return promptInstall();
       }
-      grunt.verbose.writeln(gem + ' is installed, continuing task...');
+
+      grunt.verbose.writeln('{0} is installed, continuing task...'.format(gem));
       cb();
     });
 
     // Prompt the user for the installation of the gem in case it isn't installed yet
     function promptInstall(){
       if(options.gemInstall){
-        grunt.verbose.writeln('options.gemInstall is true, so forcing the installation of ' + gem);
-        return executeCommand('gem install ' + gem, true);
+        grunt.verbose.writeln('options.gemInstall is true, so forcing the installation of {0}'.format(gem));
+        return executeCommand('gem install {0}'.format(gem), true);
       }
 
       var confirmOptions = {
         type: 'confirm', 
         name: 'install', 
-        message: 'The ' + gem + ' gem is required for grunt-xcode to work, do you wish to install it?', 
+        message: 'The {0} gem is required for grunt-xcode to work, do you wish to install it?'.format(gem), 
         default: true
       }
 
