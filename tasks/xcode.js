963614be22df7311c4bc4a80be75ec689ad50f51
Matias Singers <mail@matiassingers.com>
2014-09-30 22:04:23 +0800

    Add back String.prototype.format to xcode.js task file

--- tasks/xcode.js
+++ tasks/xcode.js
@@ -9,6 +9,16 @@
 
 var Promise = require('bluebird');
 var exec = Promise.promisify(require('child_process').exec);
+
+String.prototype.format = function() {
+    var formatted = this;
+    for (var i = 0; i < arguments.length; i++) {
+        formatted = formatted.replace(
+            RegExp('\\{' + i + '\\}', 'g'), arguments[i]);
+    }
+    return formatted;
+};
+
 module.exports = function(grunt) {
   grunt.registerMultiTask('xcode', 'Build and export Xcode projects with Grunt', function() {
     var done = this.async();
