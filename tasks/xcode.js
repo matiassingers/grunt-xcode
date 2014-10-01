c345be23ade7c76e83ea256c7896e5112892cafa
Matias Singers <mail@matiassingers.com>
2014-10-01 18:50:16 +0800

    Change executeCommand() API and style, use same style as sindresorhus' grunt-shell

--- tasks/xcode.js
+++ tasks/xcode.js
@@ -8,9 +8,9 @@
 'use strict';
 
 var Promise = require('bluebird');
-var exec = Promise.promisify(require('child_process').exec);
 var temporary = require('temporary');
 var chalk = require('chalk');
+var exec = require('child_process').exec;
 
 String.prototype.format = function() {
     var formatted = this;
@@ -26,15 +26,40 @@ function safelyWrap(string){
 }
 
 module.exports = function(grunt) {
-  function executeCommand(command){
-    grunt.verbose.writeln('Running command: {0}'.format(command));
-    return exec(command, {maxBuffer: 1000*1024})
-      .catch(handleCommandError);
-  }
-  function handleCommandError(error){
-    if (error){
-      grunt.warn(error);
-    }
+  function executeCommand(args){
+    var command = 'xcodebuild ' + args.join(' ');
+    grunt.verbose.writeln('Command:', chalk.yellow(command));
+
+    return new Promise(function(resolve, reject){
+      // See Sindre Sorhus' grunt-shell - https://github.com/sindresorhus/grunt-shell/blob/master/tasks/shell.js
+      var cmd = exec(command, {maxBuffer: 1000*1024}, function(error){
+        if(error) {
+          grunt.warn(error);
+
+          reject(error);
+        }
+
+        resolve();
+      });
+
+      var captureOutput = function (child, output) {
+        if (grunt.option('color') === false) {
+          child.on('data', function (data) {
+            output.write(chalk.stripColor(data));
+          });
+        } else {
+          child.pipe(output);
+        }
+      };
+
+      if (grunt.option('verbose')) {
+        captureOutput(cmd.stdout, process.stdout);
+      }
+
+      if (grunt.option('verbose')) {
+        captureOutput(cmd.stderr, process.stderr);
+      }
+    });
   }
 
   grunt.registerMultiTask('xcode', 'Build and export Xcode projects with Grunt', function() {
@@ -83,7 +108,9 @@ module.exports = function(grunt) {
         return Promise.resolve();
       }
 
-      var command = 'xcodebuild clean -project "{0}"'.format(options.project);
+      var command = ['clean'];
+      if(options.project) command.push('-project', safelyWrap(options.project));
+
       return executeCommand(command)
         .then(function(){
           grunt.verbose.ok('`xcodebuild clean` was successful');
@@ -94,7 +121,7 @@ module.exports = function(grunt) {
         return Promise.resolve();
       }
 
-      var command = ['xcodebuild archive'];
+      var command = ['archive'];
       command.push('-archivePath', safelyWrap(options.archivePath));
 
       if(options.project) command.push('-project', safelyWrap(options.project));
@@ -111,7 +138,7 @@ module.exports = function(grunt) {
         command.push('-alltargets');
       }
 
-      return executeCommand(command.join(' '))
+      return executeCommand(command)
         .then(function(){
           grunt.verbose.ok('`xcodebuild archive` was successful');
         });
@@ -121,8 +148,7 @@ module.exports = function(grunt) {
         return Promise.resolve();
       }
 
-      var command = ['xcodebuild'];
-      command.push('-exportArchive');
+      var command = ['-exportArchive'];
       command.push('-archivePath "{0}.xcarchive"'.format(options.archivePath));
       command.push('-exportPath "{0}/{1}"'.format(options.exportPath, options.exportFilename));
 
@@ -133,7 +159,7 @@ module.exports = function(grunt) {
       if(options.exportInstallerIdentity) command.push('-exportInstallerIdentity', safelyWrap(options.exportInstallerIdentity));
       if(!options.exportSigningIdentity) command.push('-exportWithOriginalSigningIdentity');
 
-      executeCommand(command.join(' '))
+      executeCommand(command)
         .then(function(){
           grunt.verbose.ok('`xcodebuild export` was successful');
         });
